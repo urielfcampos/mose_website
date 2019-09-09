f0ee233e18dbfc93d12bc2cae72418e21d4b8b66
urielfcampos <urielfcampos95@gmail.com>
2019-09-09 17:49:02 -0300

    Adicionado esqueleto para visualizaçao de informacoes

---
+++ layouts/public.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link class="navbar-item" :to="{ name: 'public' }">
+          <img src="@/assets/logo-mose-transp.png" alt="Logo mose" />
+        </nuxt-link>
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link class="navbar-item" :to="{ name: 'public-noticias' }">
+            Noticias
+          </nuxt-link>
+
+          <nuxt-link class="navbar-item" :to="{ name: 'public-eventos' }">
+            Eventos
+          </nuxt-link>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              Exemplos
+            </a>
+
+            <div class="navbar-dropdown">
+              <nuxt-link class="navbar-item" :to="{ name: 'public-artefatos' }">
+                Artefatos
+              </nuxt-link>
+              <nuxt-link
+                class="navbar-item"
+                :to="{ name: 'public-indicadores' }"
+              >
+                Indicadores
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <nuxt />
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
